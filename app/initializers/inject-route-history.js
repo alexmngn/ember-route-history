ac5acf49edec585b7f795807997d5035cefee20c
Alexis Mangin <me@alexismangin.com>
2015-10-27 18:45:44 -0400

    Remove ember-data that was making travis to fail and fix for route-history injection

--- app/initializers/inject-route-history.js
+++ app/initializers/inject-route-history.js
@@ -1,12 +1,1 @@
-import RouteHistoryService from 'ember-route-history/services/route-history';
-
-export function initialize(application) {
-	application.register('service:route-history', RouteHistoryService);
-	application.inject('route', 'routeHistory', 'service:route-history');
-}
-
-export default {
-	name: 'injectRouteHistory',
-	after: 'store',
-	initialize: initialize
-};
+export { default, initialize } from 'ember-route-history/initializers/inject-route-history';
